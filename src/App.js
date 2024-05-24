073bc3469d1471ea12f87361f4b74aefa8b64ea0
Sandra-Tavares <sandratavaress1972@gmail.com>
2024-05-23 21:27:39 -0300

    Criação componentes Banner e Container

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -5,11 +7,14 @@ function App() {
   return (
     < >
     <Header />
-   
+    <Banner image="assistir" />
+    <Container>
       <h2>Geografia</h2>
       lista de cards e videos
+    </Container>
 
     <Footer />
+   
     
     </>
   );
